eeeec4198f339c652c25870900fcfc660fb44fba
esryu <esryu@ucsd.edu>
2020-08-15 12:45:57 -0700

    travis

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramryuapi;
+export POSTGRES_PASSWORD=udagramryuapi;
+export POSTGRES_HOST=udagramryuapi.cutbyi1qfo2k.us-east-2.rds.amazonaws.com;
+export POSTGRES_DB=udagramryuapi;
+export AWS_BUCKET=udagram-ryu-api;
+export AWS_REGION=us-east-2;
+export AWS_PROFILE=default;
+export JWT_SECRET=helloworld;
+export URL=http://localhost:8100;
+echo "is this being processed";
